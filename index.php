a2dfe18446b04d169daef5385b4cb865ef2f4e38
alfredocacarica <alejomirarchi@gmail.com>
2025-03-26 14:13:50 -0300

    ss

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fundación Menem Zulemito</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h1>FUNDACION MENEM ZULEMITO</h1>
+            <ul>
+                <li>inicio</li>
+                <li>fin</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <article>
+            <section id="recycling">
+                <h2>Reciclaje Tecnológico</h2>
+                <p>Información sobre reciclaje tecnológico...</p>
+            </section>
+            <section id="cyberbullying">
+                <h2>Ciber Acoso</h2>
+                <p>Información sobre ciber acoso...</p>
+            </section>
+        </article>
+    </main>
+    <footer></footer>
+</body>
+</html>
